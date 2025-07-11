4ce376283d241d27e4bf17e3cd64cf0628e440c2
meetguleria <meetguleria94@gmail.com>
2025-07-11 02:47:21 -0700

    packages errors resolve

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -9,6 +12,10 @@ const config: HardhatUserConfig = {
       chainId: 42069,
       accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true",
+    currency: "USD",
   },
 };
 
